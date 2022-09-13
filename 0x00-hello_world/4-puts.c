2f19ccd9afc94dea1d506b5fda9a831f01502452
Luke Moya <lukeomondi@rocketmail.com>
2022-09-13 10:03:41 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  *
  * Description: prints "Programming is like building a multilingual puzzle"
  *
- * Return 1 if the number is positive. 0 otherwise
+ * Return: 1 if the number is positive. 0 otherwise
  */
 int main(void)
 {
