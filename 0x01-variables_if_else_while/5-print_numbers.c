d2f6c8c8ff4815e5079d5f14e9c67bfdb8ac7727
Luke Moya <lukeomondi@rocketmail.com>
2022-09-15 11:14:31 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints 0123456789
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%i", n);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
