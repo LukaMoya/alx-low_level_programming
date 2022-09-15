3f83d1ae0a55640b89fefea0bc412c2f8fee57f7
Luke Moya <lukeomondi@rocketmail.com>
2022-09-15 10:33:40 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print lowercase and uppercase using putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lower = 'a';
+	char upper = 'A';
+
+	while (lower <= 'z' ) /*print lowercase a-z*/
+	{
+		putchar(lower);
+		lower++;
+	}
+
+	while (upper <= 'Z' ) /*print uppercase A-Z*/
+	{
+		putchar(upper);
+		upper++;
+	}
+	putchar('\n');
+	return (0);
+}
