23f7df1f57a78b7ecea1d9d867cb265eea236738
Luke Moya <lukeomondi@rocketmail.com>
2022-09-21 21:18:16 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints all combination of 3 digits
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int one;
+	int ten;
+	int hundred;
+
+	for (hundred = '0'; heundred <= '9'; hundred++) /*hundreds place*/
+	{
+		for (ten = (hundred + 1); ten <= '9'; ten++) /*tens=100s+1*/
+		{
+			for (one = (ten + 1); one <= '9'; one++) /*ones*/
+			{
+				putchar(hundred);
+				putchar(ten); 
+				putchar(one);
+			       if (hundred != '7' || ten != '8' || one != '9')
+			       {
+			       		putchar(',');
+					putchar(' ');
+			       }	       
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
